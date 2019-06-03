116d60e20b86a4603c2e634512138cea0dba9188
migue <migue@MBP-de-Migue.lan>
2019-06-02 23:35:40 -0300

    login page restructure

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { updateUser } from '../../actions'
+import React, { Component } from 'react';
 
-const LoginPage = ({ dispatch }) => {
-  let input
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      surname: '',
+      email: '',
+      age: 0
+    };
+  }
 
-  return (
-    <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        dispatch(updateUser(input.value))
-        input.value = ''
-      }}>
-        <input ref={node => input = node} />
-        <button type="submit">
-          Login
-        </button>
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = (event) => {
+    alert('A user was submitted: ' + this.state.name);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
+        </label>
+        <label>
+          Surname:
+          <input type="text" name="surname" value={this.state.surname} onChange={this.handleInputChange} />
+        </label>
+        <label>
+          Email:
+          <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} />
+        </label>
+        <input type="submit" value="Submit" />
       </form>
-    </div>
-  )
+    );
+  }
 }
 
-export default connect()(LoginPage)
+export default LoginPage;
