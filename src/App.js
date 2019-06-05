3f14a7646ac5d8ecc8d211260b73a0175ff66c8f
miguelchirinos <miguel.chirinos@quadminds.com>
2019-06-05 10:24:22 -0300

    svg styling

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react';
-import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import RegularFont from './assets/fonts/Montserrat-Regular.ttf';
-import BoldFont from './assets/fonts/Montserrat-Bold.ttf';
 import './App.css';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import ProductsPage from './components/ProductsPage/ProductsPage';
 import { AppContextProvider } from './appContext'
 
-const GlobalStyles = createGlobalStyle`
-  body{
-    @font-face {
-      font-family: 'Montserrat';
-      src: url('${RegularFont}') format('opentype');
-    }
-
-    @font-face {
-      font-family: 'Montserrat-Bold';
-      src: url('${BoldFont}') format('opentype');
-    }
-
-  } 
-`
 class App extends Component {
   render() {
     return (
       <Router>
         <AppContextProvider>
-          <GlobalStyles />
           <Route path="/" exact component={MainPage} />
           <Route path="/login/" component={LoginPage} />
           <Route path="/products/" component={ProductsPage} />
--- src/components/MainPage/components/TechSection/TechSection.jsx
+++ src/components/MainPage/components/TechSection/TechSection.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import techImage from '../../../../assets/images/Ic_Tecnologys.svg';
+import { ReactComponent as Logo } from '../../../../assets/images/Ic_Tecnologys.svg';
 import styled from 'styled-components';
 
 const Hero = styled.section`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    margin: 40px 60px 40px 100px;
+    margin: 40px 60px 80px 100px;
 `;
 
 const H3 = styled.h3`
@@ -14,14 +14,10 @@ const H3 = styled.h3`
     font-size: 180%;
 `;
 
-const Img = styled.img`
-
-`;
-
 const HeroBox = () => (
     <Hero id="technologies">
         <H3>Estamos buscando<br/>para incorporar gente<br/>increíble para estas<br/>tecnologías:</H3>
-        <Img src={techImage} alt="Technologies"/>
+        <Logo/>
     </Hero>
 );
 
